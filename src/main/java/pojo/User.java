5c97e3defdaffad9156a52bca523cf30161b19f7
Jack <Dog@Ergou.com>
2019-07-01 14:13:10 +0800

    fsdfdfsfsfsdfsdffasdfasdfsafsd

--- src/main/java/pojo/User.java
+++ src/main/java/pojo/User.java
@@ -1,8 +1,7 @@
 package pojo;
 
 public class User {
-    private String nameadfasdfa;
-    private Integer ageadsfasdfsadfasf;
-    private String asdfasdfasdsnamesdfsdf;
-    private Integer asdfasdfasdfadsfagesdfadf;
+    private String name;
+    private Integer age;
+
 }
